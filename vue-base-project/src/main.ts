2e30ce37746b450bcf6645350a705cd0c64cb0b1
zhaiyongtao <501671900@qq.com>
2022-09-19 11:22:24 +0800

    fix: 初始化项目配置

--- vue-base-project/src/main.ts
+++ vue-base-project/src/main.ts
@@ -1,16 +1,22 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import Lego from "@nio-fe/lego";
-import "@nio-fe/lego/lib/style.css";
-import App from "./App.vue";
-import router from "./router";
-
-import "./assets/main.css";
-
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import lego, { defineOption } from '@nio-fe/lego';
+import { LocaleSupportLang } from '@nio-fe/locale-vue';
+import { NIcon } from '@nio-fe/icon';
+import '@nio-fe/lego/lib/style.css';
+import App from './App.vue';
+import router from './router';
+import './assets/main.css';
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
-app.use(Lego);
+app.use(router).use(
+  lego,
+  defineOption({
+    locale: {
+      current: LocaleSupportLang.ZH_CN,
+    },
+  }),
+);
 
-app.mount("#app");
+app.mount('#app');
